2ea780fd7232ac3c6d39e6e5050e9f28f32b42eb
Lucas Albertim <lucas.albertim7@gmail.com>
2023-02-23 19:52:46 -0300

    Primeiro Commit

---
+++ src/teste/BoletimEstudantil.java
@@ -0,0 +1,19 @@
+package teste;
+import java.util.Scanner;
+
+
+public class BoletimEstudantil {
+	public static void main(String[] args) {
+		try (Scanner nota = new Scanner(System.in)) {
+			int mediaFinal;
+			System.out.println("DIGITE A MEDIA ");
+			mediaFinal = nota.nextInt();		
+			if (mediaFinal < 6)
+				System.out.println("REPROVADO");
+			else if (mediaFinal == 6)
+				System.out.println("PROVA FINAL");
+			else
+				System.out.println("APROVADO");
+		}
+	}
+}
